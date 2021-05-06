d3ba48a6bc03e66d7eb4509ea06e855139e96ee1
hkl-iii <rahleggon@gmail.com>
2021-05-05 22:41:58 -0500

    added n to fmtPrintln lines 10 & 12

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ func main() {
 
 	for n := range numbers {
 		if n%2 == 0 {
-			fmt.Println("Even")
+			fmt.Println(n, " is Even")
 		} else {
-			fmt.Println("Odd")
+			fmt.Println(n, " is Odd")
 		}
 	}
 }
